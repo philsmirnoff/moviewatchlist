84f5914bc24efc1b43faf284b9d8b07e32f93504
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-10-02 15:39:57 -0400

    Sequelize models good to go

---
+++ server/db/index.js
@@ -0,0 +1,70 @@
+const Sequelize = require("sequelize");
+
+const dbConnection = new Sequelize(
+  "postgres://ilfiryakupov@localhost:5432/moviewatchlist"
+);
+
+
+// testing connection
+// const test = async () => {
+//   try {
+//   await dbConnection.authenticate();
+//   console.log("authenticate worked")
+// } catch (e) {
+//     console.error(e);
+// }
+// };
+// test();
+
+/*
+    Movie model
+        - title (not null)
+        - imdbLink (null)
+        - watched (not null, boolean, default false)
+    Genre model
+        - name (not null)
+    Many-to-many movies and genres
+    belongsToMany
+*/
+
+const Movie = dbConnection.define("movie", {
+  title: {
+    type: Sequelize.DataTypes.STRING(255),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imdbLink: {
+    type: Sequelize.DataTypes.STRING(1000),
+    allowNull: true,
+    validate: {
+        isUrl: true
+    }
+},
+watched: {
+    type: Sequelize.DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+}
+});
+
+const Genre = dbConnection.define("genre", {
+  name: {
+      type: Sequelize.DataTypes.STRING(50),
+      allowNull: false
+  }
+});
+
+Movie.belongsToMany(Genre, { through: "movies_genres" }); // setGenres
+Genre.belongsToMany(Movie, { through: "movies_genres" });
+
+
+module.exports = {
+  dbConnection: dbConnection,
+  Movie: Movie,
+  Genre: Genre
+};
+
+
+
--- server/index.js
+++ server/index.js
@@ -2,11 +2,50 @@ const express = require("express");
 const app = express();
 
 const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!~`);
-});
 
+const dbConnection = require("./db");
+
+const startServer = async () => {
+  await dbConnection.sync();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!~`);
+ });
+};
+startServer();
 
 app.get("/", (req, res) => {
   res.send("Hello :)");
+});
+
+/*
+    Movie model
+        - title (not null)
+        - imdbLink (null)
+        - watched (not null, boolean, default false)
+    Genre model
+        - name (not null)
+    Many-to-many movies and genres
+    belongsToMany
+*/
+
+const Movie = dbConnection.define("movie", {
+  title: {
+    type: Sequelize.DataTypes.STRING(255),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imdbLink: {
+    type: Sequelize.DataTypes.STRING(1000),
+    allowNull: true,
+    validate: {
+        isUrl: true
+    }
+},
+watched: {
+    type: Sequelize.DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+}
 });
